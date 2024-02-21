208071f0ca5f0251ec68d57a6bbc387ef5ccda26
LucasSpinelli003 <rm97757@fiap.com.br>
2024-02-21 15:09:22 -0300

    [Feat] - finish tests

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,5 +1,11 @@
-import 'dotenv/config'
+import { config } from 'dotenv' 
 import { z } from 'zod'
+
+if(process.env.NODE_ENV === "test"){
+  config({path: ".env.test"})
+}else{
+  config()
+}
 
 const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
@@ -11,8 +17,9 @@ const _env = envSchema.safeParse(process.env)
 
 if (_env.success === false) {
   console.error('Warning: Invalid enviroment variables: ', _env.error.format())
-
+  
   throw new Error('Invalid enviroment variables')
 }
 
 export const env = _env.data
+
--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -1,6 +1,8 @@
 import { afterAll, beforeAll, it, describe, expect } from 'vitest'
+import { execSync } from 'child_process'
 import { app } from '../src/app'
 import request from 'supertest'
+import { beforeEach } from 'node:test'
 
 describe('Transactions routes', () =>{
     beforeAll( async() =>{
@@ -9,7 +11,12 @@ describe('Transactions routes', () =>{
     afterAll( async() =>{
         await app.close()
     })
-    
+    beforeEach(() =>{
+        execSync('npm run knex migrate:rollback --all')
+        execSync('npm run knex migrate:latest')
+
+    })
+
     
     it('should be able to create a new transaction', async () =>{
     
@@ -21,6 +28,7 @@ describe('Transactions routes', () =>{
             type:'credit'
         })
         .expect(201)
+    
     })
 
     it('shoud be able to list all transactions', async () =>{
@@ -47,4 +55,69 @@ describe('Transactions routes', () =>{
             })
         ])
     })
+
+    it('shoud be able to get a specific transaction', async () =>{
+
+        const createTransactionResponse =  await request(app.server)
+         .post('/transactions')
+         .send({
+             title:'newTransaction',
+             amount:2.50,
+             type:'credit'
+         })
+ 
+         const cookie = createTransactionResponse.get('Set-Cookie')
+ 
+        const listTransactionsResponse = await request(app.server)
+         .get('/transactions')
+         .set('Cookie', cookie)
+    
+
+         const transactionId = listTransactionsResponse.body.transactions[0].id
+
+         const getSpecificTransaction = await request(app.server)
+         .get(`/transactions/${transactionId}`)
+         .set('Cookie', cookie)
+
+
+         expect(getSpecificTransaction.body.transaction).toEqual(
+             expect.objectContaining({
+                title:'newTransaction',
+                amount:2.50,
+             })
+         )
+     })
+
+     it('shoud be able to get summary of transactions', async () =>{
+
+        const createCreditTransactionResponse =  await request(app.server)
+         .post('/transactions')
+         .send({
+             title:'creditTransaction',
+             amount:5000,
+             type:'credit'
+         })
+         const cookie = createCreditTransactionResponse.get('Set-Cookie')
+
+            await request(app.server)
+         .post('/transactions')
+         .set('Cookie', cookie)
+         .send({
+             title:'debitTransaction',
+             amount:2000,
+             type:'debit'
+         })
+ 
+ 
+        const summaryTransactionsResponse = await request(app.server)
+         .get('/transactions/summary')
+         .set('Cookie', cookie)
+         .expect(200)
+         
+         expect(summaryTransactionsResponse.body.summary).toEqual(
+             expect.objectContaining({
+                 amount:3000
+             })
+         )
+     })
 })
